8736655f7b544da4f4d19c65d224cb097afb9d4b
csld <chrischenscience@gmail.com>
2024-01-24 13:17:35 +0800

    hello

---
+++ L0617.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class L0617 {
+    final static Scanner sc = new Scanner(System.in);
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 != null && root2 != null) {
+            root1.val += root2.val;
+        }
+        if (root1 != null) {
+            if (root1.left == null) {
+                if (root2.left != null) {
+                    root1.left = root2.left;
+                }
+            }
+        }
+        if(root2 != null){
+
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Time elapsed in milliseconds: " + elapsedTime);
+    }
+
+}
