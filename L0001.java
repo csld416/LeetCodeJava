bfd977b5b9ab6d45a1adf452f70ceeb79075c21e
csld <chrischenscience@gmail.com>
2024-01-24 15:35:24 +0800

    brute force

--- L0001.java
+++ L0001.java
@@ -4,7 +4,19 @@ public class L0001 {
     final static Scanner sc = new Scanner(System.in);
 
     public int[] twoSum(int[] nums, int target) {
-        return new int[0];
+        int ans[] = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if(j == i){
+                    continue;
+                }
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) throws Throwable {
