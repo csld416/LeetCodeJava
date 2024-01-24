d9a9e66851c16f6287ee0b44a64491733c10435a
csld <chrischenscience@gmail.com>
2024-01-24 15:51:17 +0800

    Convert Sorted Array to Binary Search Tree

--- L0108.java
+++ L0108.java
@@ -22,6 +22,23 @@ public class L0108 {
         }
     }
 
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        return bst(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode bst(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = bst(nums, start, mid - 1);
+        root.right = bst(nums, mid + 1, end);
+        return root;
+    }
 
     public static void main(String[] args) throws Throwable {
         long startTime = System.currentTimeMillis();
