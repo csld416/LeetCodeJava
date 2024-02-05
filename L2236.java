8b405d83f3cfb83b03c6625679667b80a2e87a89
csld <chrischenscience@gmail.com>
2024-02-05 10:30:59 +0800

    Root Equals Sum of Children

---
+++ L2236.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class L2236 {
+    final static Scanner sc = new Scanner(System.in);
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean checkTree(TreeNode root) {
+        return root.left.val + root.right.val == root.val;
+    }
+
+    public static void main(String[] args) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Time elapsed in milliseconds: " + elapsedTime);
+    }
+
+}
