0652ced1f2de8d7fe36c7c31b71d62f38e7411be
csld <chrischenscience@gmail.com>
2024-02-14 14:08:41 +0800

    Search Insert Position

---
+++ L0035.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class L0035 {
+    final static Scanner sc = new Scanner(System.in);
+
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            }
+            if (nums[m] < target) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return l + (r - l) / 2;
+    }
+
+    public static void main(String[] args) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Time elapsed in milliseconds: " + elapsedTime);
+    }
+
+}
