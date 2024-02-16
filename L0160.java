46c0c71c2a1322b848511c088b771b4d90c473bd
csld <chrischenscience@gmail.com>
2024-02-16 19:50:52 +0800

    Intersection of Two Linked Lists

---
+++ L0160.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class L0160 {
+    final static Scanner sc = new Scanner(System.in);
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
+
+    public static void main(String[] args) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Time elapsed in milliseconds: " + elapsedTime);
+    }
+
+}
