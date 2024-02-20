ab5d1d86a86d50f68cd07b007e76d4186c5cc833
csld <chrischenscience@gmail.com>
2024-02-20 09:31:37 +0800

    Missing Number

---
+++ L0268.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class L0268 {
+    final static Scanner sc = new Scanner(System.in);
+
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return nums[nums.length - 1] + 1;
+    }
+
+    public static void main(String[] args) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Time elapsed in milliseconds: " + elapsedTime);
+    }
+
+}
