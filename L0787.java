02aed31ffc185d984cf403b902a99283e001ab0a
csld <chrischenscience@gmail.com>
2024-04-18 11:38:43 +0800

    Cheapest Flights Within K Stops

---
+++ L0787.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class L0787 {
+    final static Scanner sc = new Scanner(System.in);
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int ans = 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < flights.length; i++) {
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws Throwable {
+
+        long startTime = System.currentTimeMillis();
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Time elapsed in milliseconds: " + elapsedTime);
+    }
+
+}
